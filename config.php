89c89cd5f1ef0b431860b07148c302c7ed524ed7
Henrique de Andrade <henrique@uniriotec.br>
2010-03-02 16:12:15 -0300

    ignorando o config.php

--- config.php
+++ config.php
@@ -4,20 +4,20 @@ unset($CFG);
 
 $CFG->dbtype    = 'mysql';
 $CFG->dbhost    = 'localhost';
-$CFG->dbname    = 'moodle';
-$CFG->dbuser    = 'moodle';
-$CFG->dbpass    = '123456';
+$CFG->dbname    = 'euni';
+$CFG->dbuser    = 'root';
+$CFG->dbpass    = 'asd';
 $CFG->dbpersist =  false;
 $CFG->prefix    = 'mdl_';
 
-$CFG->wwwroot   = 'http://10.4.31.134/euni';
-$CFG->dirroot   = '/home/moodle';
-$CFG->dataroot  = '/home/moodledata';
+$CFG->wwwroot   = 'http://localhost/~henrique/euni';
+$CFG->dirroot   = '/home/henrique/public_html/euni';
+$CFG->dataroot  = '/home/henrique/moodledata';
 $CFG->admin     = 'admin';
 
 $CFG->directorypermissions = 00777;  // try 02777 on a server in Safe Mode
 
-$CFG->passwordsaltmain = 'H_Q]08jW{LQ:G[NC^@U}Y`Hn:j';
+$CFG->passwordsaltmain = 'JK(~,YT[}hfF)9K(Tb @)G*Ka3[0LYy';
 
 require_once("$CFG->dirroot/lib/setup.php");
 // MAKE SURE WHEN YOU EDIT THIS FILE THAT THERE ARE NO SPACES, BLANK LINES,
